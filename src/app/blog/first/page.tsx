997cdd18c7af26c703f92292c5cddfa45cc16c1e
Tsila <tsilanmjr2@gmail.com>
2025-02-13 10:36:48 +0300

    feat: add simple blog routage

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FirstBlogPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the First Blog Page</h1>
+            <p>This is the content of the first blog page.</p>
+        </div>
+    );
+};
+
+export default FirstBlogPage;
